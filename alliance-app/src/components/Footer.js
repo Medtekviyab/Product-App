915407388622690537bce931eb01b10686d0c7a1
Yugaraj-medtek <142379784+Yugaraj-medtek@users.noreply.github.com>
2023-12-26 17:14:23 +0530

    UPDATED LANDING PAGE AND ADDED FOOTER

--- alliance-app/src/components/Footer.js
+++ alliance-app/src/components/Footer.js
@@ -3,13 +3,13 @@ import React from "react";
 const Footer = () => {
   return (
     <div>
-      <footer class="w-full p-8 mt-10 bg-white border-gray-200 shadow md:flex md:items-center md:justify-between dark:bg-gray-900 dark:border-gray-700">
+      <footer class="w-full p-8 mt-10 bg-gray-900 md:flex md:items-center md:justify-between dark:bg-gray-900 dark:border-gray-700">
         <span class="text-3xl ml-24 text-white lg:text-center dark:text-white">
          QUOT BOT
         </span>
         <ul class="flex flex-wrap items-center mt-3 text-md font-medium text-white dark:text-white sm:mt-0">
           <li>
-            <a href="#" class="hover:underline mr-20">
+            <a href="#" class=" flex hover:underline mr-20">
               Contact us : quotbotsupport@gmail.com
             </a>
           </li>
